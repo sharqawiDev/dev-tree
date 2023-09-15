9c74dfe0a4bdb2c0fefa29f429922a64285a365d
sharqawidev <sharqawidev@gmail.com>
2023-09-15 17:47:25 +0300

    frontend: adding basic styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+const Main = () => {
+  return (
+    <div style={{ width: "100%" }}>
+      <p>Hello</p>
+    </div>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default Main;
